2b0827594be2453952702d6a45ccbcba634f3643
Evan.Chang <evan.chang@akmii.com>
2016-11-23 14:07:33 +0800

    添加本地版本

---
+++ sh/bendi/js/datajs/minsu.js
@@ -0,0 +1,85 @@
+﻿var dataUrl = [
+{
+    "id": "1",
+    "subject": "上海城市民俗",
+    "title": "《西服店》",
+    "image": "image/templateimg/ms/1.西服店.jpg",
+    "timelength": "13",
+    "team": "啼声（上海）影视文化有限公司",
+    "content": "讲述白领程晓光与他爷爷穿过上海的大街小巷，共同寻找着一家老西服店……",
+    "urlid": "C98E2087033CB0EF9C33DC5901307461"
+},
+{
+    "id": "2",
+    "subject": "上海城市民俗",
+    "title": "《棕绷师傅》",
+    "image": "image/templateimg/ms/2.棕绷师傅.jpg",
+    "timelength": "6",
+    "team": "真实传媒有限公司",
+    "content": "讲述了老上海的人文文化，怎样制作棕绷的过程及这门手艺。",
+    "urlid": "1C5036C40DDF56059C33DC5901307461"
+},
+{
+    "id": "3",
+    "subject": "上海城市民俗",
+    "title": "《鸽子王》",
+    "image": "image/templateimg/ms/3.鸽子王.jpg",
+    "timelength": "6",
+    "team": "真实传媒有限公司",
+    "content": "讲述了一个上海人物的小故事，一个地道的上海人养鸽子的心路历程。",
+    "urlid": "6E58C66C93C6F9E59C33DC5901307461"
+},
+{
+    "id": "4",
+    "subject": "上海城市民俗",
+    "title": "《天弦琴梦》",
+    "image": "image/templateimg/ms/4.天弦琴梦.jpg",
+    "timelength": "13",
+    "team": "上海美肯文化传媒有限公司",
+    "content": "讲述了上海本地的一名二胡演奏家的故事。",
+    "urlid": "EE741052789A7BD29C33DC5901307461"
+}
+,
+{
+    "id": "5",
+    "subject": "上海城市民俗",
+    "title": "《我们的中国梦——未来篇》",
+    "image": "image/templateimg/ms/5.我们的中国梦——未来篇.jpg",
+    "timelength": "5",
+    "team": "上海东方电视购物有限公司",
+    "content": "记录了三个在上海的生活小片段，互相信任的自助报摊；用心去承担责任的敬老院院长；有心就能创新的老爷爷，这一幕幕温馨感人的片段，是上海美好生活的写照。",
+    "urlid": "68EB77786227A6979C33DC5901307461"
+}
+,
+{
+    "id": "6",
+    "subject": "上海城市民俗",
+    "title": "《阿大的葱油饼》",
+    "image": "image/templateimg/ms/6.阿大的葱油饼.jpg",
+    "timelength": "7",
+    "team": "朱亦敏",
+    "content": "讲述了一个上海大叔制作葱油饼的故事。",
+    "urlid": "2E461D8EA5ADA10A9C33DC5901307461"
+},
+{
+    "id": "7",
+    "subject": "上海城市民俗",
+    "title": "《记忆中的棕绷店》",
+    "image": "image/templateimg/ms/7.记忆中的棕绷店.jpg",
+    "timelength": "4",
+    "team": "上海文广互动电视有限公司",
+    "content": "上海棕绷师傅的故事。",
+    "urlid": "4883C8D5CF1581C69C33DC5901307461"
+}
+,
+{
+    "id": "8",
+    "subject": "上海城市民俗",
+    "title": "做提琴的人",
+    "image": "image/templateimg/ms/8.做提琴的人.jpg",
+    "timelength": "6",
+    "team": "真实传媒有限公司",
+    "content": "刻刀下，发现平凡中的匠人心——85岁的李贵福是建国第一代提琴制作师，他与徒弟二人用纯手工的打磨、制作，让每一把琴都独一无二。",
+    "urlid": "A4C1BD1C3D93837D9C33DC5901307461"
+}
+]
